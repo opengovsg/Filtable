0353875bc4cc6d380437ef1d01388c269dc92c73
raynerljm <raynerljm@gmail.com>
2023-01-25 15:04:39 +0800

    refactor: rename filters-sg to filtable

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
+import { ThemeProvider } from "@opengovsg/design-system-react";
 import { type AppType } from "next/dist/shared/lib/utils";
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 };
 
 export default MyApp;
