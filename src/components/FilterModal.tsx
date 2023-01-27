6a4724d9dc432079246f46c5575fa4e314da717f
raynerljm <raynerljm@gmail.com>
2023-01-27 21:10:17 +0800

    fix: make filters only apply when clicking apply

--- src/components/FilterModal.tsx
+++ src/components/FilterModal.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 // Components
 import {
   Modal,
@@ -32,73 +33,87 @@ const FilterModal: FC<Props> = ({
   setFilter,
   processedFilters,
 }) => {
+  const [temporaryFilter, setTemporaryFilter] = useState(filter);
+
   const allFilterOptions = enumerateAllFilterOptions(
     listings,
     processedFilters
   );
 
+  const closeAndResetFilters = () => {
+    setTemporaryFilter(filter);
+    onClose();
+  };
+
+  const closeAndApplyFilters = () => {
+    setFilter(temporaryFilter);
+    onClose();
+  };
+
   return (
-    <Modal isOpen={isOpen} onClose={onClose} size="full">
+    <Modal isOpen={isOpen} onClose={closeAndResetFilters} size="full">
       <ModalOverlay />
       <ModalContent p="24px" pt="42px">
         <ModalCloseButton top="42px" right="24px" />
         <Text textStyle="h5" mb="26px">
           All filters
         </Text>
-        {Object.entries(allFilterOptions["Checkbox"] ?? {}).map((y, idx) => {
-          const [checkboxName, set] = y;
-          return (
-            <Box key={checkboxName} py="16px">
-              <>
-                <Text textStyle="subhead-2" mb="8px">
-                  {checkboxName}
-                </Text>
-                <Box display="flex" flexDir="row" flexWrap="wrap" gap="8px">
-                  {Array.from(set as Set<string>).map((option) => {
-                    const isSelected = ((filter["Checkbox"] ?? {})[
-                      checkboxName
-                    ] ?? {})[option];
-                    const handleToggle = () => {
-                      setFilter({
-                        ...filter,
-                        Checkbox: {
-                          ...filter["Checkbox"],
-                          [checkboxName]: {
-                            ...filter["Checkbox"][checkboxName],
-                            [option]: !((filter["Checkbox"] ?? {})[
-                              checkboxName
-                            ] ?? {})[option],
+        {Object.entries(allFilterOptions["Checkbox"] ?? {}).map(
+          (entries, idx) => {
+            const [checkboxName, set] = entries;
+            return (
+              <Box key={checkboxName} py="16px">
+                <>
+                  <Text textStyle="subhead-2" mb="8px">
+                    {checkboxName}
+                  </Text>
+                  <Box display="flex" flexDir="row" flexWrap="wrap" gap="8px">
+                    {Array.from(set as Set<string>).map((option) => {
+                      const isSelected = ((temporaryFilter["Checkbox"] ?? {})[
+                        checkboxName
+                      ] ?? {})[option];
+                      const handleToggle = () => {
+                        setTemporaryFilter((tempFilter) => ({
+                          ...tempFilter,
+                          Checkbox: {
+                            ...tempFilter["Checkbox"],
+                            [checkboxName]: {
+                              ...tempFilter["Checkbox"][checkboxName],
+                              [option]: !((tempFilter["Checkbox"] ?? {})[
+                                checkboxName
+                              ] ?? {})[option],
+                            },
                           },
-                        },
-                      });
-                    };
+                        }));
+                      };
 
-                    return (
-                      <Tag
-                        key={`${checkboxName}-${option}`}
-                        variant={isSelected ? "solid" : "subtle"}
-                        minW="fit-content"
-                        whiteSpace="nowrap"
-                        cursor="pointer"
-                        display="flex"
-                        flexDirection="row"
-                        gap="4px"
-                        alignItems="center"
-                        colorScheme={getTagColorScheme(idx)}
-                        onClick={handleToggle}
-                      >
-                        {isSelected ? <BxCheck /> : null}
-                        <Text textStyle="body-2">{option}</Text>
-                      </Tag>
-                    );
-                  })}
-                </Box>
-              </>
-            </Box>
-          );
-        })}
+                      return (
+                        <Tag
+                          key={`${checkboxName}-${option}`}
+                          variant={isSelected ? "solid" : "subtle"}
+                          minW="fit-content"
+                          whiteSpace="nowrap"
+                          cursor="pointer"
+                          display="flex"
+                          flexDirection="row"
+                          gap="4px"
+                          alignItems="center"
+                          colorScheme={getTagColorScheme(idx)}
+                          onClick={handleToggle}
+                        >
+                          {isSelected ? <BxCheck /> : null}
+                          <Text textStyle="body-2">{option}</Text>
+                        </Tag>
+                      );
+                    })}
+                  </Box>
+                </>
+              </Box>
+            );
+          }
+        )}
         {/* TODO: Add apply filters functionality */}
-        <Button mt="auto" onClick={onClose}>
+        <Button mt="auto" onClick={closeAndApplyFilters}>
           Apply filters
         </Button>
       </ModalContent>
