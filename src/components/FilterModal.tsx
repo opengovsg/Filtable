eeaae64ad4220febb1fac67892a2042dacaf967a
raynerljm <raynerljm@gmail.com>
2023-01-30 18:21:57 +0800

    fix: fix filter modal not taking up full screen

--- src/components/FilterModal.tsx
+++ src/components/FilterModal.tsx
@@ -61,7 +61,7 @@ const FilterModal: FC<Props> = ({
   return (
     <Modal isOpen={isOpen} onClose={closeAndResetFilters} size="full">
       <ModalOverlay />
-      <ModalContent p="24px" pt="42px" minH={"fill-available"}>
+      <ModalContent p="24px" pt="42px" minH={"fill-available"} h="100%">
         <ModalCloseButton top="42px" right="24px" />
         <Text textStyle="h5" mb="26px">
           All filters
