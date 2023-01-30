53cd54038f12d95ae9d26f1eee95077dde741faf
raynerljm <raynerljm@gmail.com>
2023-01-30 17:55:59 +0800

    feat: change all headings to optional headings

--- src/types/headings.ts
+++ src/types/headings.ts
@@ -1,7 +1,12 @@
-export const mandatoryHeadings = ["Filtable Title", "Title"] as const;
+export const mandatoryHeadings = [] as const;
 export type MandatoryHeadings = (typeof mandatoryHeadings)[number];
 
-export const optionalHeadings = ["Description", "Link URL"] as const;
+export const optionalHeadings = [
+  "Filtable Title",
+  "Title",
+  "Description",
+  "Link URL",
+] as const;
 export type OptionalHeadings = (typeof optionalHeadings)[number];
 
 export const headings = [...mandatoryHeadings, ...optionalHeadings] as const;
